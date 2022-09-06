9be0d849b48b4fe2e9bbfa99ccfc163172b6a9bc
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-09-07 01:41:18 +0300

    Add plus and minus functionality on shoe quantity in cart

--- src/index.js
+++ src/index.js
@@ -87,21 +87,75 @@ document.addEventListener("DOMContentLoaded",() => {
                     btn.textContent = "Add to cart"
 
                     btn.addEventListener("click",() => {
+                        console.log("clicked");
+                        let quantity = 1;
+                        let shoePrice = 0;
+                        const cartElement = document.createElement("article");
+                        cartElement.classList.add("cart-element");
+                        const cartElementHeader = document.createElement("div");
+                        cartElementHeader.classList.add("cart-element-header");
                         const shoeName = document.createElement("h4");
                         shoeName.textContent = name;
-                        const div = document.createElement("div");
-                        div.classList.add("quantity-price");
+                        const trashSpan = document.createElement("span");
+                        const trashIcon = document.createElement("i");
+                        trashIcon.classList.add("fa-solid");
+                        trashIcon.classList.add("fa-trash");
+                        const quantityPrice = document.createElement("div");
+                        quantityPrice.classList.add("quantity-price");
+                        const quantityDiv = document.createElement("div");
+                        quantityDiv.classList.add("quantity");
+
+                        const quantityIcons = document.createElement("span");
+                        quantityIcons.classList.add("quantity-icons");
+                        const minusSpan = document.createElement("span");
+                        const minus = document.createElement("i");
+                        minus.classList.add("fa-solid");
+                        minus.classList.add("fa-minus");
+
+                        minusSpan.addEventListener("click",() => {
+                            quantity--;
+                            quantityP.textContent = `x${quantity}`;
+                            shoePrice = quantity * price;
+                            priceP.textContent = `$${shoePrice}`;
+                        })
+
+                        const plusSpan = document.createElement("span");
+                        const plus = document.createElement("i");
+                        plus.classList.add("fa-solid");
+                        plus.classList.add("fa-plus");
+
+                        plusSpan.addEventListener("click",() => {
+                            quantity++;
+                            quantityP.textContent = `x${quantity}`;
+                            shoePrice = quantity * price;
+                            priceP.textContent = `$${shoePrice}`;
+                        })
+
                         const quantityP = document.createElement("p");
-                        quantityP.textContent = "x1";
+                        quantityP.textContent = `x${quantity}`;
                         const priceP = document.createElement("p");;
                         priceP.textContent = `$${price}`;
                         const hr = document.createElement("hr");
                         hr.classList.add("line");
-                        div.appendChild(quantityP);
-                        div.appendChild(priceP);
-                        cartDetails.appendChild(shoeName);
-                        cartDetails.appendChild(div)
-                        cartDetails.appendChild(hr);
+                        
+                        cartElementHeader.appendChild(shoeName);
+                        trashSpan.appendChild(trashIcon);
+                        cartElementHeader.appendChild(trashSpan);
+                        quantityDiv.appendChild(quantityP);
+                        
+                        minusSpan.appendChild(minus);
+                        quantityIcons.appendChild(minusSpan);
+                        plusSpan.appendChild(plus);
+                        quantityIcons.appendChild(plusSpan);
+                        quantityDiv.appendChild(quantityIcons)
+                        quantityPrice.appendChild(quantityDiv);
+                        quantityPrice.appendChild(priceP);
+                        
+                        cartElement.appendChild(cartElementHeader);
+                        cartElement.appendChild(quantityPrice);
+                        cartElement.appendChild(hr);
+
+                        cartDetails.appendChild(cartElement);
                     })
 
                     anotherArticle.appendChild(h4);
